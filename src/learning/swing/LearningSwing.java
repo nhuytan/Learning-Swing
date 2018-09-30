f48a6bcb061a3c8cc4bd921c3a6cf0a8dce6d2dc
nhuytan <42280131+nhuytan@users.noreply.github.com>
2018-09-30 00:18:18 -0500

    Initial commit

---
+++ src/learning/swing/LearningSwing.java
@@ -0,0 +1,14 @@
+package learning.swing;
+
+import javax.swing.JFrame;
+
+public class LearningSwing {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("JFrame demo");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+    }
+}
